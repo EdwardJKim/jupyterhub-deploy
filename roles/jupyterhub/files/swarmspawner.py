bbcf67b744d0065f66b21f16901c55d7ec8e8618
Edward J Kim <edward.junhyung.kim@gmail.com>
2015-07-22 18:49:38 +0000

    make changes for our course

--- roles/jupyterhub/files/swarmspawner.py
+++ roles/jupyterhub/files/swarmspawner.py
@@ -1,5 +1,7 @@
 from tornado import gen
 from dockerspawner import DockerSpawner, SystemUserSpawner
+import os
+from traitlets import Unicode
 
 # urllib3 complains that we're making unverified HTTPS connections to swarm,
 # but this is ok because we're connecting to swarm via 127.0.0.1. I don't
@@ -11,9 +13,36 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 
-class SwarmSpawner(SystemUserSpawner):
+class SwarmSpawner(DockerSpawner):
 
     container_ip = '0.0.0.0'
+
+    home_mount = Unicode('/media/nfs/home', config=True)
+    singleuser = Unicode('data_scientist', config=True)
+
+    @property
+    def volume_binds(self):
+        """
+        The second half of declaring a volume with docker-py happens when you
+        actually call start().  The required format is a dict of dicts that
+        looks like:
+        {
+            host_location: {'bind': container_location, 'ro': True}
+        }
+        """
+        volumes = {
+            key: {'bind': value, 'ro': False}
+            for key, value in self.volumes.items()
+        }
+        ro_volumes = {
+            key: {'bind': value, 'ro': True}
+            for key, value in self.read_only_volumes.items()
+        }
+        volumes[os.path.join(self.home_mount, self.user.name, 'assignments')] = (
+            '/home/{}/assignments'.format(self.singleuser)
+        )
+        volumes.update(ro_volumes)
+        return volumes
 
     @gen.coroutine
     def lookup_node_name(self):
@@ -29,8 +58,8 @@ class SwarmSpawner(SystemUserSpawner):
     def start(self, image=None, extra_create_kwargs=None):
         # look up mapping of node names to ip addresses
         info = yield self.docker('info')
-        num_nodes = int(info['DriverStatus'][2][1])
-        node_info = info['DriverStatus'][3::5]
+        num_nodes = int(info['DriverStatus'][3][1])
+        node_info = info['DriverStatus'][4::5]
         self.node_info = {}
         for i in range(num_nodes):
             node, ip_port = node_info[i]
@@ -41,9 +70,11 @@ class SwarmSpawner(SystemUserSpawner):
         if extra_create_kwargs is None:
             extra_create_kwargs = {}
         if 'mem_limit' not in extra_create_kwargs:
-            extra_create_kwargs['mem_limit'] = '1g'
+            extra_create_kwargs['mem_limit'] = '512m'
         if 'working_dir' not in extra_create_kwargs:
-            extra_create_kwargs['working_dir'] = self.homedir
+            extra_create_kwargs['working_dir'] = '/home/data_scientist'
+        if 'hostname' not in extra_create_kwargs:
+            extra_create_kwargs['hostname'] = 'temporary_hostname'
         yield DockerSpawner.start(
             self, image=image, extra_create_kwargs=extra_create_kwargs)
 
--- roles/jupyterhub/templates/jupyterhub_config.py
+++ roles/jupyterhub/templates/jupyterhub_config.py
@@ -5,30 +5,43 @@ import os
 import sys
 
 # Base configuration
-c.JupyterHub.log_level = "INFO"
-c.JupyterHub.db_url = 'sqlite:////srv/jupyterhub_db/jupyterhub.sqlite'
+c.JupyterHub.log_level = "DEBUG"
+#c.JupyterHub.db_url = 'sqlite:////srv/jupyterhub_db/jupyterhub.sqlite'
+c.JupyterHub.log_file = "/srv/jupyterhub/jupyterhub.log"
+
+c.JupyterHub.cookie_secret_file = "/srv/jupyterhub/cookie_secret_file"
+c.JupyterHub.proxy_auth_token = "{{ configproxy_auth_token }}"
 
 # Configure the authenticator
-c.JupyterHub.authenticator_class = 'docker_oauth.DockerOAuthenticator'
-c.DockerOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-c.DockerOAuthenticator.create_system_users = True
-c.Authenticator.admin_users = admin = set()
-c.Authenticator.whitelist = whitelist = set()
+c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
+
+c.LocalGitHubOAuthenticator.github_client_id = "{{ github_client_id }}"
+c.LocalGitHubOAuthenticator.github_client_secret = "{{ github_client_secret }}"
+c.LocalGitHubOAuthenticator.oauth_callback_url = "{{ oauth_callback_url }}"
+c.LocalGitHubOAuthenticator.create_system_users = True
 
 # Configure the spawner
 c.JupyterHub.spawner_class = 'swarmspawner.SwarmSpawner'
-c.SystemUserSpawner.container_image = 'compmodels/systemuser'
+c.DockerSpawner.container_image = 'edwardjkim/singleuser'
 c.DockerSpawner.tls_cert = '{{ docker_tls_path }}/cert.pem'
 c.DockerSpawner.tls_key = '{{ docker_tls_path }}/key.pem'
 c.DockerSpawner.remove_containers = True
-
+c.DockerSpawner.read_only_volumes = {
+    '/media/nfs/data': '/home/data_scientist/data',
+    '/media/nfs/lessons': '/home/data_scientist/lessons',
+    '/media/nfs/testing': '/home/data_scientist/testing',
+}
 # The docker instances need access to the Hub, so the default loopback port
 # doesn't work:
 c.JupyterHub.hub_ip = '{{ servicenet_ip }}'
 
 # Add users to the admin list, the whitelist, and also record their user ids
-root = os.environ.get('OAUTHENTICATOR_DIR', os.path.dirname(__file__))
-sys.path.insert(0, root)
+#root = os.environ.get('OAUTHENTICATOR_DIR', os.path.dirname(__file__))
+#sys.path.insert(0, root)
+c.Authenticator.admin_users = admin = set()
+c.Authenticator.whitelist = whitelist = set()
+
+root = '/srv/jupyterhub'
 
 with open(os.path.join(root, 'userlist')) as f:
     for line in f:
