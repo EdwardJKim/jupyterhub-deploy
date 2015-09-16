7af75e3a75ce447ff0b6f1fc142a33d87121b34f
Edward J Kim <edward.junhyung.kim@gmail.com>
2015-09-16 02:12:33 +0000

    edit jupyterhub config

--- roles/jupyterhub/templates/jupyterhub_config.py
+++ roles/jupyterhub/templates/jupyterhub_config.py
@@ -5,39 +5,39 @@ import os
 import sys
 
 # Base configuration
-c.JupyterHub.log_level = "DEBUG"
-#c.JupyterHub.db_url = 'sqlite:////srv/jupyterhub_db/jupyterhub.sqlite'
+c.JupyterHub.log_level = "INFO"
+c.JupyterHub.db_url = 'sqlite:////srv/jupyterhub/jupyterhub.sqlite'
 c.JupyterHub.log_file = "/srv/jupyterhub/jupyterhub.log"
 
 c.JupyterHub.cookie_secret_file = "/srv/jupyterhub/cookie_secret_file"
 c.JupyterHub.proxy_auth_token = "{{ configproxy_auth_token }}"
 
 # Configure the authenticator
-c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
-
+c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
 c.LocalGitHubOAuthenticator.github_client_id = "{{ github_client_id }}"
 c.LocalGitHubOAuthenticator.github_client_secret = "{{ github_client_secret }}"
 c.LocalGitHubOAuthenticator.oauth_callback_url = "{{ oauth_callback_url }}"
-c.LocalGitHubOAuthenticator.create_system_users = True
 
 # Configure the spawner
 c.JupyterHub.spawner_class = 'swarmspawner.SwarmSpawner'
 c.DockerSpawner.container_image = 'edwardjkim/singleuser'
 c.DockerSpawner.tls_cert = '{{ docker_tls_path }}/cert.pem'
 c.DockerSpawner.tls_key = '{{ docker_tls_path }}/key.pem'
-c.DockerSpawner.remove_containers = True
 c.DockerSpawner.read_only_volumes = {
+    '/media/nfs/rp-pdss15': '/home/data_scientist/rp-pdss15 (read-only)',
+    '/media/nfs/rp-pdm15': '/home/data_scientist/rp-pdm15 (read-only)',
     '/media/nfs/data': '/home/data_scientist/data',
-    '/media/nfs/lessons': '/home/data_scientist/lessons',
-    '/media/nfs/testing': '/home/data_scientist/testing',
+    '/media/nfs/database/schema': '/home/data_scientist/database/schema'
 }
+c.DockerSpawner.hub_ip_connect = '{{ servicenet_ip }}'
+
 # The docker instances need access to the Hub, so the default loopback port
 # doesn't work:
+c.JupyterHub.ip = '{{ servicenet_ip }}'
+c.JupyterHub.proxy_api_ip = '{{ servicenet_ip }}'
 c.JupyterHub.hub_ip = '{{ servicenet_ip }}'
 
 # Add users to the admin list, the whitelist, and also record their user ids
-#root = os.environ.get('OAUTHENTICATOR_DIR', os.path.dirname(__file__))
-#sys.path.insert(0, root)
 c.Authenticator.admin_users = admin = set()
 c.Authenticator.whitelist = whitelist = set()
 
@@ -50,5 +50,4 @@ with open(os.path.join(root, 'userlist')) as f:
         parts = line.split()
         name = parts[0]
         whitelist.add(name)
-        if len(parts) > 1 and parts[1] == 'admin':
-            admin.add(name)
+        admin.add('EdwardJKim')
